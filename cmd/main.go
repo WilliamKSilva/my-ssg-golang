5ce264060a79c5a89ddf71e0d0c67d963e3afd9b
WilliamKSilva <wilz.kelvin12@gmail.com>
2025-07-27 16:58:11 -0300

    feat: change directory content reading to be made on the main function and passed to other functions as needed

--- cmd/main.go
+++ cmd/main.go
@@ -25,12 +25,26 @@ type ArticlePreviewData struct {
 }
 
 func main() {
-	err := parseMarkdown()
+	htmlFiles, err := os.ReadDir("content/gen")
+	if err != nil {
+		// Create the dir to store generated Markdown HTML content if does not exist
+		err := os.Mkdir("content/gen", 0755)
+		if err != nil {
+			log.Fatalf("[ERROR] error trying to create 'gen' directory: %v", err)
+		}
+	}
+
+	contentFiles, err := os.ReadDir("content")
+	if err != nil {
+		log.Fatalf("[ERROR] error trying to read 'content' directory: %v", err)
+	}
+
+	err = parseMarkdown(contentFiles)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = buildArticlesPage()
+	err = buildArticlesPage(htmlFiles)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,27 +60,27 @@ func main() {
 	}
 }
 
-func parseMarkdown() error {
-	dirs, err := os.ReadDir("content")
-	if err != nil {
-		return fmt.Errorf("[ERROR] error trying to read markdown files from dir: %v", err)
-	}
-
-	for _, d := range dirs {
-		if d.IsDir() {
+func parseMarkdown(contentFiles []os.DirEntry) error {
+	contentExists := false
+	for _, f := range contentFiles {
+		if f.IsDir() {
 			continue
 		}
 
-		fExtensionSplit := strings.Split(d.Name(), ".")
+		fExtensionSplit := strings.Split(f.Name(), ".")
 		if len(fExtensionSplit) > 0 {
 			fExtension := fExtensionSplit[1]
 
 			if fExtension == "json" {
 				continue
 			}
+
+			if fExtension == "md" {
+				contentExists = true
+			}
 		}
 
-		b, err := os.ReadFile(fmt.Sprintf("content/%s", d.Name()))
+		b, err := os.ReadFile(fmt.Sprintf("content/%s", f.Name()))
 		if err != nil {
 			log.Printf("[ERROR] error trying to read .md file: %v", err)
 			continue
@@ -78,7 +92,7 @@ func parseMarkdown() error {
 			continue
 		}
 
-		fName := strings.Replace(d.Name(), ".md", "", 3)
+		fName := strings.Replace(f.Name(), ".md", "", 3)
 
 		err = os.WriteFile(fmt.Sprintf("content/gen/%s.html", fName), buf.Bytes(), 0755)
 		if err != nil {
@@ -87,15 +101,14 @@ func parseMarkdown() error {
 		}
 	}
 
+	if !contentExists {
+		return fmt.Errorf("[ERROR] no Markdown file found to be parsed on 'content' directory")
+	}
+
 	return nil
 }
 
-func buildArticlesPage() error {
-	dirs, err := os.ReadDir("content/gen")
-	if err != nil {
-		return fmt.Errorf("[ERROR] error trying to read gen HTML files: %v", err)
-	}
-
+func buildArticlesPage(htmlFiles []os.DirEntry) error {
 	// Populate the Article HTML template for each generated
 	// HTML article from the Markdown files
 	t, err := template.ParseFiles("templates/article.html")
@@ -103,10 +116,10 @@ func buildArticlesPage() error {
 		return fmt.Errorf("[ERROR] error trying to allocate new template HTML page: %v", err)
 	}
 
-	for _, d := range dirs {
-		fName := strings.Replace(d.Name(), ".html", "", 5)
+	for _, f := range htmlFiles {
+		fName := strings.Replace(f.Name(), ".html", "", 5)
 
-		b, err := os.ReadFile(fmt.Sprintf("content/gen/%s", d.Name()))
+		b, err := os.ReadFile(fmt.Sprintf("content/gen/%s", f.Name()))
 		if err != nil {
 			log.Printf("[ERROR] error trying to read gen HTML file: %v", err)
 			continue
